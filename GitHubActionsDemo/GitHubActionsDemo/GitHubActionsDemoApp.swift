baa76a552cdde46623a6a17b811e42cff5ba1c18
IKEH1024 <pondau1024@gmail.com>
2022-06-23 16:25:20 +0900

    create project

---
+++ GitHubActionsDemo/GitHubActionsDemo/GitHubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsDemoApp.swift
+//  GitHubActionsDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
